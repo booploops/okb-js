d25c15910efd9b72536494fdea40aa2303ae5839
booploops <49113086+booploops@users.noreply.github.com>
2025-08-13 01:46:34 -0700

    added d.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,37 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import dts from "vite-plugin-dts";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), cssInjectedByJsPlugin()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    cssInjectedByJsPlugin(),
+    dts({
+      rollupTypes: true,
+      tsconfigPath: './tsconfig.app.json'
+    }),
+  ],
   define: {
-    'process.env': {}
+    "process.env": {},
   },
   build: {
     lib: {
-      entry: 'src/main.ts', // adjust to your entry file
-      name: 'OKB',
+      entry: "src/main.ts", // adjust to your entry file
+      name: "OKB",
       fileName: (format) => `okb.${format}.js`,
-      formats: ['es', 'iife']
+      formats: ["es", "iife"],
     },
     rollupOptions: {
       // Externalize dependencies if needed
       external: [],
       shimMissingExports: true,
       output: {
-        format: 'esm',
-        globals: {}
-      }
-    }
+        format: "esm",
+        globals: {},
+      },
+    },
   },
-})
+});
