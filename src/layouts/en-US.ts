3ca4e7c8afb66f82c0963c992eaf5fcf7c19e652
booploops <49113086+booploops@users.noreply.github.com>
2025-10-01 21:15:33 -0700

    Standardized symbols

--- src/layouts/en-US.ts
+++ src/layouts/en-US.ts
@@ -9,15 +9,15 @@ import { KeyboardLayout } from "./KeyboardLayout";
 
 export default new KeyboardLayout({
     normal: [
-        "1 2 3 4 5 6 7 8 9 0 - =",
-        "q w e r t y u i o p [ ] \\",
-        "a s d f g h j k l : ; '",
-        "z x c v b n m , . /",
+        "1 2 3 4 5 6 7 8 9 0",
+        "q w e r t y u i o p",
+        "a s d f g h j k l",
+        "z x c v b n m",
     ],
     shift: [
-        "1 2 3 4 5 6 7 8 9 0 - =",
-        "Q W E R T Y U I O P { } |",
-        "A S D F G H J K L + : \"",
-        "Z X C V B N M , . /",
+        "1 2 3 4 5 6 7 8 9 0",
+        "Q W E R T Y U I O P",
+        "A S D F G H J K L",
+        "Z X C V B N M",
     ]
 });
