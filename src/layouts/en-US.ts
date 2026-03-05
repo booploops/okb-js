7b02a20d812d5d8e8a03ccae415a8abe79232f55
booploops <49113086+booploops@users.noreply.github.com>
2026-03-04 16:42:04 -0800

    Added , . / to keyboard

--- src/layouts/en-US.ts
+++ src/layouts/en-US.ts
@@ -15,13 +15,13 @@ export default new KeyboardLayout({
         "1 2 3 4 5 6 7 8 9 0",
         "q w e r t y u i o p",
         "a s d f g h j k l",
-        "z x c v b n m",
+        "z x c v b n m , . /",
     ],
     shift: [
         "1 2 3 4 5 6 7 8 9 0",
         "Q W E R T Y U I O P",
         "A S D F G H J K L",
-        "Z X C V B N M",
+        "Z X C V B N M , . /",
     ],
     shiftLock: false
 });
